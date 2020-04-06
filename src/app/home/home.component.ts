0042169718a43eb327643bbd863523d0003a3ab6
annabelhayes <annabel.hayes@googlemail.com>
2020-04-06 13:28:42 +0100

    feat(home): add develop svg animation on hover

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  @ViewChild('bracket1', { static: false }) bracket1: ElementRef;
+  @ViewChild('bracket2', { static: false }) bracket2: ElementRef;
+  @ViewChild('line', { static: false }) line: ElementRef;
+
+
+  onMouseEnterDevelop() {
+    this.bracket1.nativeElement.classList.add('bracket1-animation');
+    this.bracket2.nativeElement.classList.add('bracket2-animation');
+    this.line.nativeElement.classList.add('line-animation');
+  }
 }
