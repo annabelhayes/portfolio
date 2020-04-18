cedca48ae6bae953ebf13abf65a4b423197c99cc
annabelhayes <annabel.hayes@googlemail.com>
2020-04-18 12:20:47 +0100

    feat(design): create placeholder component

---
+++ src/app/design/design.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-design',
+  templateUrl: './design.component.html',
+  styleUrls: ['./design.component.css']
+})
+
+export class DesignComponent {
+
+}
+
