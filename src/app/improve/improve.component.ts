044dc4ee33a126d434f2162f55276c18c3833777
annabelhayes <annabel.hayes@googlemail.com>
2020-04-18 12:21:00 +0100

    feat(improve): create placeholder component

---
+++ src/app/improve/improve.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-improve',
+  templateUrl: './improve.component.html',
+  styleUrls: ['./improve.component.css']
+})
+
+export class ImproveComponent {
+
+}
+
---
+++ src/app/improve/improve.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ImproveComponent } from './improve.component';
+
+@NgModule({
+  declarations: [ImproveComponent],
+  imports: [CommonModule],
+  exports: [ImproveComponent],
+})
+export class ImproveModule { }
