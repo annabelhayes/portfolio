73a6da89e7839a34c4e5e11167cb11894472847e
annabelhayes <annabel.hayes@googlemail.com>
2020-04-18 12:20:39 +0100

    feat(create): create placeholder component

---
+++ src/app/create/create.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CreateComponent } from './create.component';
+
+@NgModule({
+  declarations: [CreateComponent],
+  imports: [CommonModule],
+  exports: [CreateComponent],
+})
+export class CreateModule { }
