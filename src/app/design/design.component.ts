0533fc643bddd881e826354c0f8287d876a4aef8
annabelhayes <annabel.hayes@googlemail.com>
2020-05-03 13:21:52 +0100

    feat(design): add  intro content

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 @Component({
   selector: 'app-design',
   templateUrl: './design.component.html',
   styleUrls: ['./design.component.css']
 })
 
-export class DesignComponent {
+export class DesignComponent implements OnInit {
+
+  ngOnInit() {
+    AOS.init();
+  }
+
+  scrollToElement($element): void {
+    $element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+  }
 
 }
 
--- src/app/design/design.module.ts
+++ src/app/design/design.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DesignComponent } from './design.component';
+import { NavComponent } from '../nav/nav.component';
 
 @NgModule({
-  declarations: [DesignComponent],
+  declarations: [DesignComponent, NavComponent],
   imports: [CommonModule],
   exports: [DesignComponent],
 })
