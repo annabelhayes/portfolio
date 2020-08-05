7773f70293733b9efff6b9d3b14a83e8a0adf689
annabelhayes <annabel.hayes@googlemail.com>
2020-08-05 22:10:54 +0100

    feat(create): add placeholder content

--- src/app/components/create/create.module.ts
+++ src/app/components/create/create.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CreateComponent } from './create.component';
+import { NavComponent } from 'src/app/nav/nav.component';
 
 @NgModule({
-  declarations: [CreateComponent],
+  declarations: [CreateComponent, NavComponent],
   imports: [CommonModule],
   exports: [CreateComponent],
 })
